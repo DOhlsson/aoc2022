e741b649a850f9080729c7e64f1dbfee252ed7e9
Daniel Ohlsson <dohlsson89@gmail.com>
2022-12-06 23:00:15 +0100

    Day 6 part 2

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -6,21 +6,37 @@ pub fn day6(input: Box<dyn BufRead>) {
     let mut lines = input.lines();
     let line = lines.next().unwrap().unwrap();
 
-    let mut ans: usize = 0;
-    'search: for i in 0..line.len() - 3 {
-        let slice = &line[i..i + 4];
-        let mut iter = slice.chars();
+    let mut part1: Option<usize> = None;
+    let mut part2: Option<usize> = None;
 
-        for _ in 0..3 {
-            let item = iter.next().unwrap();
-            if iter.clone().find(|it| it.eq(&item)).is_some() {
-                continue 'search;
+    let mut iter = line.chars();
+    'search: for i in 0.. {
+        iter.next();
+
+        if part1.is_none() {
+            let mut iter_clone = iter.clone().take(4);
+            for _ in 0..3 {
+                let item = iter_clone.next().unwrap();
+                if iter_clone.clone().find(|it| it.eq(&item)).is_some() {
+                    continue 'search;
+                }
             }
+            part1 = Some(i + 4);
         }
 
-        ans = i + 4;
-        break 'search;
+        if part2.is_none() {
+            let mut iter_clone = iter.clone().take(14);
+            for _ in 0..13 {
+                let item = iter_clone.next().unwrap();
+                if iter_clone.clone().find(|it| it.eq(&item)).is_some() {
+                    continue 'search;
+                }
+            }
+            part2 = Some(i + 14);
+            break 'search;
+        }
     }
 
-    println!("Part 1: {}", ans);
+    println!("Part 1: {}", part1.unwrap());
+    println!("Part 2: {}", part2.unwrap());
 }
